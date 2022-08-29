0b123dcb00c90eb611f127c549d7329d7b9bfc7b
BalazsGitHub <balazs.dombovari@gmail.com>
2022-08-29 14:03:10 +0200

    Initial commit

---
+++ src/main/java/com/example/githubaction1/GithubAction1Application.java
@@ -0,0 +1,13 @@
+package com.example.githubaction1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubAction1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubAction1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubaction1/GithubAction1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubaction1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubAction1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
